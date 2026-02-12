377b1f87dfbe8b120506c401800b2d9a1d5b3be9
procoder443 <>
2026-02-12 12:39:08 +0500

    test

--- src/pages/InvoiceScreen.jsx
+++ src/pages/InvoiceScreen.jsx
@@ -408,7 +408,7 @@ function InvoiceScreen() {
               children="Find"
               color="blue"
               onClick={() => setIsJobModalOpen(true)}
-              style={{ marginBottom: "12px", height: "50px" }}
+              style={{ marginBottom: "15px", height: "50px" }}
             />
           </div>
         </div>
