8eb0af37fdc819a7203c52c9ff36a9439930c843
procoder443 <>
2026-02-17 18:10:01 +0500

    test

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -67,17 +67,34 @@ const Footer = () => {
       </div>
       {/* Center Icon */}
 
-      <div className="flex flex-col items-center -mt-10">
+      {/* <div className="flex flex-col items-center -mt-10">
         <div
                   onClick={() => navigate("/schedule")}
 
         
         className="w-17 h-17 rounded-full bg-[#1980d4] border-[8px] border-[#D3DCE5] flex items-center justify-center flex-col shadow-md">
           <FiCalendar size={15} className="text-white" />
-          {/* <Image src={i8} /> */}
          <p className="text-white text-[7px] mt-1">Schedule Job</p>
         </div>
          
+      </div> */}
+
+
+      <div className="flex flex-col items-center -mt-10">
+        <div
+          onClick={() => navigate("/schedule")}
+          /* 1. Text removed from inside here. 
+             2. Icon size increased. 
+             3. border-white used to blend with footer (optional) */
+          className="w-16 h-16 rounded-full bg-[#1980d4] border-[6px] border-white flex items-center justify-center shadow-lg"
+        >
+          <FiCalendar size={24} className="text-white" />
+        </div>
+        
+        {/* Text is now OUTSIDE the blue button */}
+        <span className="text-xs mt-2 text-[#5290C1] font-medium">
+          Schedule
+        </span>
       </div>
       {/* Chase */}
       <div
